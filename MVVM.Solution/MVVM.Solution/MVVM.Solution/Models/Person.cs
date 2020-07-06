c51f22da3925f7b226725e67e0903a0d135c9671
Resul Kurtulmuşlu <www.prometer.com.tr:5880>
2020-07-06 21:54:15 +0300

    mvvm başlangıç

---
+++ MVVM.Solution/MVVM.Solution/MVVM.Solution/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM.Solution.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        //public string Name { get; set; }
+        private string _name { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Surname { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MVVM.Solution/MVVM.Solution/MVVM.Solution/Views/PersonPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using MVVM.Solution.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM.Solution.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonPage : ContentPage
+    {
+        //private readonly List<Person> people = new List<Person>();
+        private readonly IList<Person> people = new ObservableCollection<Person>(); // Ekleme ,silme ve reflesh durumlarında değişiklikleri hiçbirşeye ihtiyaç duymadan ekrana yansıtır.
+
+        public PersonPage()
+        {
+            InitializeComponent();
+            BindData();
+        }
+
+        private void BindData()
+        {
+            people.Add(new Person
+            {
+                Id = 1,
+                Name = "Resul",
+                Surname = "Kurtulmuşlu"
+            });
+
+            people.Add(new Person
+            {
+                Id = 2,
+                Name = "Çoşkun",
+                Surname = "Akdemir"
+            });
+
+            people.Add(new Person
+            {
+                Id = 3,
+                Name = "Osman",
+                Surname = "Demir"
+            });
+
+            people.Add(new Person
+            {
+                Id = 4,
+                Name = "Melek",
+                Surname = "Alice"
+            });
+
+            //1 - lst_Person.ItemsSource = people;
+            lst_Person.BindingContext = people;
+        }
+
+        private void btn_Delete_Clicked(object sender, EventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            Person selectedModel = (Person)item.CommandParameter;
+
+            if (selectedModel != null)
+                people.Remove(selectedModel);
+        }
+
+        private void btn_Update_Clicked(object sender, EventArgs e)
+        {
+            MenuItem item = (MenuItem)sender;
+            Person selectedModel = (Person)item.CommandParameter;
+
+            if (selectedModel != null)
+            {
+                selectedModel.Name += ".i";
+            }
+        }
+    }
+}
